18c02693f77b8d7e00cca14a4517706756c28a6f
Jahongiroo7 <jakhongirmurodillavey@gmail.com>
2026-04-16 12:18:39 +0500

    pagination qo'shildi va kichik o'zgarishlar bo'ldi

--- Application/mainapp/views.py
+++ Application/mainapp/views.py
@@ -2,9 +2,14 @@ from django.shortcuts import render
 
 from django.urls import path, include
 from django.contrib.auth.models import User
-from rest_framework import routers, serializers, viewsets
+from rest_framework import routers, serializers, viewsets, pagination
 from .models import Worker, Application, Category
 from .serializers import ApplicationSerializer, CategorySerializer, WorkerSerializer
+
+class our_pagination(pagination.PageNumberPagination):
+    page_size = 3
+    page_size_query_param = 'page_size'
+    max_page_size = 10000
 
 class ApplicationViewSet(viewsets.ModelViewSet):
     queryset = Application.objects.filter(status='qabul_qilingan')
@@ -17,3 +22,4 @@ class CategoryViewSet(viewsets.ModelViewSet):
 class WorkerViewSet(viewsets.ModelViewSet):
     queryset = Worker.objects.filter(status=True)
     serializer_class = WorkerSerializer
+    pagination_class = our_pagination
